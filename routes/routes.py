ddbc1d96fc720ea3c3962d8f4acde1230b387da2
Leandro Félix <58048297+lehinfo-felix@users.noreply.github.com>
2023-06-11 14:34:38 -0300

    Add files via upload

--- routes/routes.py
+++ routes/routes.py
@@ -1,14 +1,6 @@
-# Coloquei o 'todos' aqui pois não estava sabendo fazer importação de outro arquivo para renderizar
-todos = [
-    {1 : "Levar o gato para passear"},
-    {2 : "Comprar tinta spray"},
-    {3 : "Estudar Node"},
-    {4 : "Estudar Ruby"},
-    {5 : "Terminar validação de eventos API do Im Here"},
-    {6 : "Fazer salvamento local dos dados da API em SQLite no próprio app android"}
-]
+from flask import Blueprint, render_template, request, redirect
 
-from flask import Blueprint, render_template
+from data.data import todos
 
 routes_bp = Blueprint("routes", __name__)
 
@@ -20,14 +12,46 @@ def index():
 def listaTarefas():
     return render_template("listar.html", page_title='Lista de Tarefas', todos=todos)
 
-@routes_bp.route("/adicionar-tarefa")
+@routes_bp.route("/adicionar-tarefa", methods=["GET", "POST"])
 def adicionarTarefa():
+    if request.method == 'POST':
+        if request.form['to-do'] == '':
+            return render_template("adicionar.html", page_title='Adicionar Tarefas', error='Campo obrigatório')
+        todo = request.form['to-do']
+        todo = {
+            'id': len(todos) + 1,
+            'name': todo,
+            'completed': False
+        }
+        todos.append(todo)
+
     return render_template("adicionar.html", page_title='Adicionar Tarefas')
 
-@routes_bp.route("/concluir-tarefa/<id>")
-def concluirTarefa():
-    return render_template("concluir.html", page_title='Concluir Tarefas')
+@routes_bp.route("/concluir-tarefa/<int:id>", methods=["POST"])
+def concluirTarefa(id):
+    for todo in todos:
+        if todo['id'] == int(id):
+            if todo['completed'] == False:
+                todo['completed'] = True
+            else:
+                todo['completed'] = False
+            break
+    return redirect('/concluir-tarefa')
 
-@routes_bp.route("/excluir-tarefa/<id>")
-def excluirTarefa(): 
-    return render_template("excluir.html", page_title='Excluir Tarefas')
+@routes_bp.route("/concluir-tarefa", methods=["GET"])
+def concluirTarefaGet():
+    return render_template("concluir.html", page_title='Concluir Tarefas', todos=todos)
+
+@routes_bp.route("/excluir-tarefa", methods=["GET"])
+def excluirTarefaGet():
+    return render_template("excluir.html", page_title='Excluir Tarefas', todos=todos)
+
+@routes_bp.route("/excluir-tarefa/<int:id>", methods=["POST"])
+def excluirTarefa(id):
+    for todo in todos:
+        if todo['id'] == int(id):
+            todos.remove(todo)
+            break
+    for i, todo in enumerate(todos):
+        todo['id'] = i + 1
+    return redirect('/excluir-tarefa')
